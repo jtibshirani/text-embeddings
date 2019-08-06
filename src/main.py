e921936b19d5013541f192a3cd39db51266ad174
Julie Tibshirani <julie.tibshirani@elastic.co>
2019-08-06 16:22:57 -0700

    Remove the smaller data files.

--- src/main.py
+++ src/main.py
@@ -116,7 +116,7 @@ def embed_text(text):
 INDEX_NAME = "posts"
 INDEX_FILE = "data/posts/index.json"
 
-DATA_FILE = "data/posts/posts_100.json"
+DATA_FILE = "data/posts/posts.json"
 BATCH_SIZE = 1000
 
 SEARCH_SIZE = 5
