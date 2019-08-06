a2684e860e6c364ad7420a42c2456c2733287bdb
Julie Tibshirani <julie.tibshirani@elastic.co>
2019-08-05 17:16:17 -0700

    Run an auto-formatter over the main script.

--- src/main.py
+++ src/main.py
@@ -6,63 +6,65 @@ import tensorflow as tf
 import tensorflow_hub as hub
 import time
 
+
 def reindex_docs():
-  print("Creating the 'posts' index.")
-  client.indices.delete(index=INDEX_NAME, ignore=[404])
+    print("Creating the 'posts' index.")
+    client.indices.delete(index=INDEX_NAME, ignore=[404])
 
-  with open(INDEX_FILE) as file:
-    source = file.read().strip()
-    client.indices.create(index=INDEX_NAME, body=source)
+    with open(INDEX_FILE) as file:
+        source = file.read().strip()
+        client.indices.create(index=INDEX_NAME, body=source)
 
-  index_documents()
+    index_documents()
+
 
 def index_documents():
-  print("Indexing documents...")
+    print("Indexing documents...")
 
-  bulk_requests = []
-  titles = []
-  index = 0
+    bulk_requests = []
+    titles = []
+    index = 0
 
-  with open(DATA_FILE) as file:
-    while True:
-      line = file.readline().strip()
-      if not line:
-        break
+    with open(DATA_FILE) as file:
+        while True:
+            line = file.readline().strip()
+            if not line:
+                break
 
-      source = json.loads(line)
-      if source['type'] != 'question':
-        continue
+            source = json.loads(line)
+            if source["type"] != "question":
+                continue
 
-      source.update({'_op_type': 'index',
-        '_index': INDEX_NAME,
-        '_id': index})
-      
-      bulk_requests.append(source)
-      titles.append(source['title'])
-      index += 1
+            source.update({"_op_type": "index", "_index": INDEX_NAME, "_id": index})
 
-      if index % BATCH_SIZE == 0:
-        index_batch(index, bulk_requests, titles)
-        bulk_requests = []
-        titles = []
+            bulk_requests.append(source)
+            titles.append(source["title"])
+            index += 1
 
-    if bulk_requests:
-      index_batch(index, bulk_requests, titles)
+            if index % BATCH_SIZE == 0:
+                index_batch(index, bulk_requests, titles)
+                bulk_requests = []
+                titles = []
 
-  client.indices.refresh(index=INDEX_NAME)
-  print('Done indexing.')
+        if bulk_requests:
+            index_batch(index, bulk_requests, titles)
+
+    client.indices.refresh(index=INDEX_NAME)
+    print("Done indexing.")
+
 
 def index_batch(index, bulk_requests, titles):
-  print("Calculating embeddings for batch...")
-  title_vectors = embed_text(titles)
+    print("Calculating embeddings for batch...")
+    title_vectors = embed_text(titles)
 
-  for i, request in enumerate(bulk_requests):
-    request['title_vector'] = title_vectors[i]
+    for i, request in enumerate(bulk_requests):
+        request["title_vector"] = title_vectors[i]
 
-  print("Indexing documents...")
-  bulk(client, bulk_requests)
+    print("Indexing documents...")
+    bulk(client, bulk_requests)
 
-  print("Indexed {} documents.".format(index))
+    print("Indexed {} documents.".format(index))
+
 
 def embed_text(text):
     vectors = session.run(tensors, feed_dict={text_ph: text})
@@ -70,56 +72,61 @@ def embed_text(text):
 
 
 def start_query_loop():
-  while True:
-    query = input('Enter query: ')
+    while True:
+        query = input("Enter query: ")
 
-    if query == 'quit':
-      break
+        if query == "quit":
+            break
 
-    encoding_start = time.time()
+        encoding_start = time.time()
 
-    query_vector = embed_text([query])[0]
-    encoding_time = time.time() - encoding_start
+        query_vector = embed_text([query])[0]
+        encoding_time = time.time() - encoding_start
 
-    script_query = {
-      "script_score": {
-        "query": {
-          "match_all": {}
-        },
-        "script": {
-          "source": "cosineSimilarity(params.query_vector, doc['title_vector']) + 1.0",
-          "params": {
-            "query_vector": query_vector
-          }
+        script_query = {
+            "script_score": {
+                "query": {"match_all": {}},
+                "script": {
+                    "source": "cosineSimilarity(params.query_vector, doc['title_vector']) + 1.0",
+                    "params": {"query_vector": query_vector},
+                },
+            }
         }
-      }
-    }
 
-    search_start = time.time()
-    response = client.search(index=INDEX_NAME, body={'size': 5,
-      'query': script_query,
-      '_source': { 'includes': ['title', 'body']}})
-    search_time = time.time() - search_start
+        search_start = time.time()
+        response = client.search(
+            index=INDEX_NAME,
+            body={
+                "size": 5,
+                "query": script_query,
+                "_source": {"includes": ["title", "body"]},
+            },
+        )
+        search_time = time.time() - search_start
 
-    print()
-    print("{} total hits.".format(response['hits']['total']['value']))
-    print("Encoding time: {:.2f} ms".format(encoding_time * 1000), "search time: {:.2f} ms".format(search_time * 1000))
-    for hit in response['hits']['hits']:
-      print('id: {}, score: {}'.format(hit['_id'], hit['_score']))
-      print(hit['_source'])
-      print()
+        print()
+        print("{} total hits.".format(response["hits"]["total"]["value"]))
+        print(
+            "Encoding time: {:.2f} ms".format(encoding_time * 1000),
+            "search time: {:.2f} ms".format(search_time * 1000),
+        )
+        for hit in response["hits"]["hits"]:
+            print("id: {}, score: {}".format(hit["_id"], hit["_score"]))
+            print(hit["_source"])
+            print()
+
 
 ##### MAIN SCRIPT #####
 
-INDEX_NAME = 'posts'
-INDEX_FILE = 'data/posts/index.json'
+INDEX_NAME = "posts"
+INDEX_FILE = "data/posts/index.json"
 
-DATA_FILE = 'data/posts/posts_100.json'
+DATA_FILE = "data/posts/posts_100.json"
 BATCH_SIZE = 100
 
 print("Downloading pre-trained embeddings from tensorflow hub.")
 use_encoder = hub.Module("https://tfhub.dev/google/universal-sentence-encoder/2")
-#elmo_encoder = hub.Module("https://tfhub.dev/google/elmo/2", trainable=False)
+# elmo_encoder = hub.Module("https://tfhub.dev/google/elmo/2", trainable=False)
 
 text_ph = tf.placeholder(tf.string)
 tensors = use_encoder(text_ph)
@@ -135,4 +142,3 @@ reindex_docs()
 start_query_loop()
 
 session.close()
-
